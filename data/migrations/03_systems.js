87a35e516827e960577f721fb57d16118f8cd432
taterntots <matthew.a.weidner@gmail.com>
2021-05-19 18:48:17 -0400

    created a table for systems

---
+++ data/migrations/03_systems.js
@@ -0,0 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('systems', tbl => {
+      tbl.uuid('id')
+        .primary()
+        .notNullable()
+        .defaultTo(knex.raw('uuid_generate_v4()'));
+      tbl
+        .text('name')
+        .notNullable();
+      tbl
+        .timestamp('created_at')
+        .notNullable()
+        .defaultTo(knex.raw('now()'));
+      tbl
+        .timestamp('updated_at')
+        .notNullable()
+        .defaultTo(knex.raw('now()'));
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('systems');
+};
