8b5ef5ea33e9029520754d37a0d77beb940f6981
taterntots <matthew.a.weidner@gmail.com>
2021-05-19 17:31:41 -0400

    created a games table

---
+++ data/migrations/02_games.js
@@ -0,0 +1,29 @@
+exports.up = function (knex) {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('games', tbl => {
+      tbl.uuid('id')
+        .primary()
+        .notNullable()
+        .defaultTo(knex.raw('uuid_generate_v4()'));
+      tbl
+        .text('name')
+        .notNullable();
+      tbl
+        .date('release_date')
+      tbl
+        .text('banner_pic_URL');
+      tbl
+        .timestamp('created_at')
+        .notNullable()
+        .defaultTo(knex.raw('now()'));
+      tbl
+        .timestamp('updated_at')
+        .notNullable()
+        .defaultTo(knex.raw('now()'));
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('games');
+};
