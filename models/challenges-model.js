6e0fa985252431044526988d8d2739dc3152fdcf
taterntots <matthew.a.weidner@gmail.com>
2021-05-20 16:44:04 -0400

    fixed challenges model for finding all and individual challenges to display joined table data and not just IDs

--- models/challenges-model.js
+++ models/challenges-model.js
@@ -2,14 +2,64 @@ const db = require('../data/dbConfig.js');
 
 //FIND ALL CHALLENGES
 function findChallenges() {
-  return db('challenges')
+  return db('challenges as c')
+    .leftOuterJoin('users as u', 'c.user_id', 'u.id')
+    .leftOuterJoin('games as g', 'c.game_id', 'g.id')
+    .leftOuterJoin('systems as s', 'c.system_id', 's.id')
+    .leftOuterJoin('difficulty as d', 'c.difficulty_id', 'd.id')
+    .select([
+      'c.id as challenge_id',
+      'c.name',
+      'c.description',
+      'u.username',
+      'g.name as game_title',
+      'g.banner_pic_URL',
+      's.name as system',
+      'd.name as difficulty',
+      'd.points',
+      'c.rules',
+      'c.is_high_score',
+      'c.is_speedrun',
+      'c.featured',
+      'c.prize',
+      'c.start_date',
+      'c.end_date',
+      'c.created_at',
+      'c.updated_at'
+    ])
+    .groupBy('c.id', 'u.id', 'g.id', 's.id', 'd.id')
 }
 
 //FIND CHALLENGE BY ID
 function findChallengeById(challengeId) {
   return db('challenges as c')
+    .leftOuterJoin('users as u', 'c.user_id', 'u.id')
+    .leftOuterJoin('games as g', 'c.game_id', 'g.id')
+    .leftOuterJoin('systems as s', 'c.system_id', 's.id')
+    .leftOuterJoin('difficulty as d', 'c.difficulty_id', 'd.id')
     .where('c.id', challengeId)
     .first()
+    .select([
+      'c.id as challenge_id',
+      'c.name',
+      'c.description',
+      'u.username',
+      'g.name as game_title',
+      'g.banner_pic_URL',
+      's.name as system',
+      'd.name as difficulty',
+      'd.points',
+      'c.rules',
+      'c.is_high_score',
+      'c.is_speedrun',
+      'c.featured',
+      'c.prize',
+      'c.start_date',
+      'c.end_date',
+      'c.created_at',
+      'c.updated_at'
+    ])
+    .groupBy('c.id', 'u.id', 'g.id', 's.id', 'd.id')
 }
 
 //FIND CHALLENGES BY A SPECIFIC FILTER (NEEDED FOR VALIDATION MIDDLEWARE)
